5d084f6efb940b1bd0ef2d790ae0d45188a9942d
Ahmad_Setiaji-(AMDS) <54342235+AMDS-CODE-DEVELOPMENT@users.noreply.github.com>
2021-06-08 17:02:32 +0800

    deklarasi variable dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[]args) {
+
+        String namaDepan = "Ahmad";
+        String namaTengah = "Aji";
+        String namaBelakang = "Setiaji";
+
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+    }
+}
