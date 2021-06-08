c6431777c83e1777dd595a5848ebace8ada16f4b
Ahmad_Setiaji-(AMDS) <54342235+AMDS-CODE-DEVELOPMENT@users.noreply.github.com>
2021-06-08 17:06:57 +0800

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -11,5 +11,16 @@ public class DataTypes {
         char nilaiAbjad = 'A';
         boolean tampan = true;
 
+        System.out.println("Nama Depan\t: "+namaDepan);
+        System.out.print("Nama Belakang\t: ");
+        System.out.println(namaBelakang);
+
+        System.out.println("Nama Lengkap\t: "+namaDepan+" "+namaTengah+" "+namaBelakang);
+        System.out.println("Usia\t: "+usia);
+        System.out.println("Usia Saat Lulus (String)\t: "+usia+targetTahunKuliah);
+        int usiaLulus = usia+targetTahunKuliah;
+        System.out.println("Usia Saat Lulus (Variable)\t: "+usiaLulus);
+        System.out.println("Usia Saat Lulus (Kurung)\t: "+(usia+targetTahunKuliah));
+
     }
 }
