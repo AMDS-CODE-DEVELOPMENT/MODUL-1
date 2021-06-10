20c10755b700e33f50b584321180bc9d02962e94
Ahmad_Setiaji-(AMDS) <54342235+AMDS-CODE-DEVELOPMENT@users.noreply.github.com>
2021-06-10 11:50:37 +0800

    Operasi Tipe String

--- PraktikumJava1-D/src/Main.java
+++ PraktikumJava1-D/src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Ahmad";
         String namaTengah = "Aji";
         String namaBelakang = "Setiaji";
-        int usia = 18;
+        int usia = 20;
         int targetTahunKuliah = 4;
         double ipk = 3.99;
         char nilaiAbjad = 'A';
@@ -14,5 +16,28 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan+" "+namaTengah+" "+namaBelakang;
+        System.out.println(namaLengkap);
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("aw"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,12));
+        System.out.println(namaLengkap.replace("Yog", "Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray){
+            System.out.println(nama);
+        }
+
+
+
+
+
+
     }
 }
