b22542bef504a38afd6362242ae0bb815834bfed
Ahmad_Setiaji-(AMDS) <54342235+AMDS-CODE-DEVELOPMENT@users.noreply.github.com>
2021-06-10 11:11:58 +0800

    Project IDEA

---
+++ PraktikumJava1-D/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Ahmad";
+        String namaTengah = "Aji";
+        String namaBelakang = "Setiaji";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.99;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan+namaTengah+namaBelakang);
+    }
+}
